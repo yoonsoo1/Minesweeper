38fdc672de70438ae6810e56925aee651931a54f
Yoonsoo Nam <yoonsoon@usc.edu>
2022-09-19 16:09:48 -0700

    Custom tool bar added

--- app/src/main/java/com/example/minesweeper/MainActivity.java
+++ app/src/main/java/com/example/minesweeper/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.minesweeper;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.gridlayout.widget.GridLayout;
 
 import android.content.res.Resources;
@@ -13,8 +14,9 @@ import android.widget.TextView;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
+    // Code borrowed from In Class activities
     private static final int COLUMN_COUNT = 8;
+    private static final int BOMB_COUNT = 4;
 
     // save the TextViews of all cells in an array, so later on,
     // when a TextView is clicked, we know which cell it is
@@ -30,14 +32,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Toolbar toolbar = findViewById(R.id.mytoolbar);
+        setSupportActionBar(toolbar);
+
         cell_tvs = new ArrayList<TextView>();
         GridLayout grid = (GridLayout) findViewById(R.id.gridLayout01);
-        // Method (3): add four dynamically created cells with LayoutInflater
         LayoutInflater li = LayoutInflater.from(this);
         for (int i = 0; i<10; i++) {
             for (int j=0; j<8; j++) {
                 TextView tv = (TextView) li.inflate(R.layout.custom_cell_layout, grid, false);
-                //tv.setText(String.valueOf(i)+String.valueOf(j));
+                tv.setText(String.valueOf(i)+String.valueOf(j));
                 tv.setTextColor(Color.GRAY);
                 tv.setBackgroundColor(Color.GRAY);
                 tv.setOnClickListener(this::onClickTV);
@@ -67,12 +71,11 @@ public class MainActivity extends AppCompatActivity {
         int n = findIndexOfCellTextView(tv);
         int i = n/COLUMN_COUNT;
         int j = n%COLUMN_COUNT;
-        tv.setText(String.valueOf(i)+String.valueOf(j));
-        if (tv.getCurrentTextColor() == Color.GRAY) {
-            tv.setTextColor(Color.GREEN);
+        if (tv.getCurrentTextColor() == Color.GREEN) {
+            tv.setTextColor(Color.GRAY);
             tv.setBackgroundColor(Color.parseColor("lime"));
         }else {
-            tv.setTextColor(Color.GRAY);
+            tv.setTextColor(Color.GREEN);
             tv.setBackgroundColor(Color.LTGRAY);
         }
     }
